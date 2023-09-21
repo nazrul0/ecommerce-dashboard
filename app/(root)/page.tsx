6db452ff344c148fdd8091af7c5cfdfc6ffa1ea7
nazrul0 <nazrulsyahmi00@gmail.com>
2023-09-21 19:37:03 +0800

    imported clerk authentication

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
     return (
-      <p>Hello Admin Dashboard!</p>
+      <div className="p-4">
+        <Button size="default" variant="default">Click me</Button>
+      </div>
     )
   }
   
